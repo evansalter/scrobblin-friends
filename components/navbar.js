d399a125d128702bac576b72539ae7e56cd96ec1
Evan Salter <esalter@vendasta.com>
2017-01-23 18:51:31 -0600

    Add navbar component

---
+++ components/navbar.js
@@ -0,0 +1,9 @@
+Vue.component('navbar', {
+    template: `
+    <nav>
+        <div class="nav-wrapper">
+            <a href="/" class="brand-logo">Scrobblin' Friends</a>
+        </div>
+    </nav>
+    `
+});
