cf90eba394e6e5dc0b83e4781466b7857b2f8073
Evan Salter <esalter@vendasta.com>
2017-01-24 11:23:37 -0600

    appengine-ify

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+const PORT = process.env.PORT || 8080;
+
 app.get('/static/*', function(request, response){ 
      console.log('static file request : ' + request.params[0]);
      response.sendFile( __dirname + '/static/' + request.params[0]); 
@@ -10,4 +12,4 @@ app.get('*', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
-app.listen('8000');
+app.listen(PORT);
