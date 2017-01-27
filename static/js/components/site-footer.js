a4f46d89b94087d6d0c515b1b59e0b3441f2f25c
Evan Salter <esalter@vendasta.com>
2017-01-26 19:07:30 -0600

    Add site footer

---
+++ static/js/components/site-footer.js
@@ -0,0 +1,14 @@
+Vue.component('site-footer', {
+    template: `
+    <footer class="page-footer">
+        <div class="footer-copyright">
+            <div class="container">
+                Made with <i class="fa fa-coffee" aria-hidden="true"></i> by Evan Salter
+                <a class="grey-text text-lighten-4 right" href="https://github.com/esalter-va/scrobblin-friends" target="_blank">
+                    <i id="github-footer" class="fa fa-github" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+    </footer>
+    `
+})
