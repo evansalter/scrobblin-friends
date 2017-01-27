0b68b296e9a6344bf7786d8828a9a79a576b6370
Evan Salter <esalter@vendasta.com>
2017-01-27 08:56:17 -0600

    Add help message if you don't follow any users

--- static/js/components/friend-list.js
+++ static/js/components/friend-list.js
@@ -1,26 +1,31 @@
 Vue.component('friend-list', {
     props: ['friends', 'recenttracks'],
     template: `
-    <ul class="collapsible popout" data-collapsible="accordion">
-        <li v-for="friend in friends">
-            <div class="collapsible-header">
-                <div class="friend-row">
-                    <img v-bind:src="friend.imageUrl" class="profile-image circle">
-                    <span class="name">{{ friend.realname ? friend.realname : friend.name }}</span>
-                    <span class="username" v-if="friend.realname">{{ friend.name }}</span>
-                </div>
-                <span v-if="recenttracks[friend.name]" class="latest-track">
-                    <span>
-                        <i v-if="recenttracks[friend.name][0].nowplaying" class="play-arrow material-icons">play_arrow</i>
-                        {{ recenttracks[friend.name][0].name }}
+    <div>
+        <ul v-if="friends.length > 0" class="collapsible popout" data-collapsible="accordion">
+            <li v-for="friend in friends">
+                <div class="collapsible-header">
+                    <div class="friend-row">
+                        <img v-bind:src="friend.imageUrl" class="profile-image circle">
+                        <span class="name">{{ friend.realname ? friend.realname : friend.name }}</span>
+                        <span class="username" v-if="friend.realname">{{ friend.name }}</span>
+                    </div>
+                    <span v-if="recenttracks[friend.name]" class="latest-track">
+                        <span>
+                            <i v-if="recenttracks[friend.name][0].nowplaying" class="play-arrow material-icons">play_arrow</i>
+                            {{ recenttracks[friend.name][0].name }}
+                        </span>
+                        <span class="artist">{{ recenttracks[friend.name][0].artist }}</span>
                     </span>
-                    <span class="artist">{{ recenttracks[friend.name][0].artist }}</span>
-                </span>
-            </div>
-            <div class="collapsible-body">
-                <track-list v-bind:tracks="recenttracks[friend.name]"></track-list>
-            </div>
-        </li>
-    </ul>
+                </div>
+                <div class="collapsible-body">
+                    <track-list v-bind:tracks="recenttracks[friend.name]"></track-list>
+                </div>
+            </li>
+        </ul>
+        <div v-else class="row top-margin jumbo-text">
+            Follow some users on <a href="https://last.fm" target="_blank">Last.fm</a> to keep track of their listening here!
+        </div>
+    </div>
     `
 });
