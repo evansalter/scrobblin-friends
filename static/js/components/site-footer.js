1cba9f3776966cb1282f1b556a82d23ca87b8c8b
esalter-va <evanearlsalter@gmail.com>
2018-03-08 15:25:01 -0600

    Add website link

--- static/js/components/site-footer.js
+++ static/js/components/site-footer.js
@@ -3,7 +3,7 @@ Vue.component('site-footer', {
     <footer class="page-footer">
         <div class="footer-copyright">
             <div class="container">
-                Made with <i class="fa fa-coffee" aria-hidden="true"></i> by Evan Salter
+                Made with <i class="fa fa-coffee" aria-hidden="true"></i> by <a target="_blank" class="grey-text text-lighten-3" href="http://www.evansalter.com/">Evan Salter</a>
                 <a class="grey-text text-lighten-4 right" href="https://github.com/esalter-va/scrobblin-friends" target="_blank">
                     <i id="github-footer" class="fa fa-github" aria-hidden="true"></i>
                 </a>
