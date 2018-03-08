da55f6c335fe59145dcfc7338bf1e62cf48d00ad
esalter-va <evanearlsalter@gmail.com>
2018-03-08 15:11:27 -0600

    Make now playing animation work in Firefox

--- static/js/components/now-playing.js
+++ static/js/components/now-playing.js
@@ -1,7 +1,7 @@
 Vue.component('now-playing', {
     template: `
     <svg height="20px" width="20px">
-        <rect v-for="i in 3" class="bar"></rect>
+        <rect v-for="i in 3" class="bar" height="100%" width="4px"></rect>
     </svg>
     `
 });
